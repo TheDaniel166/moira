761056017fc4f7e7773f160ba7e484859d810a43
TheDaniel166 <burkettdaniel927@gmail.com>
2026-03-27 16:59:30 -0400

    bump version to 0.1.4 in facade.py and pyproject.toml

--- moira/facade.py
+++ moira/facade.py
@@ -1055,7 +1055,7 @@ __all__ = [
 try:
     __version__ = package_version("moira-astro")
 except PackageNotFoundError:
-    __version__ = "0.1.3"
+    __version__ = "0.1.4"
 __author__  = "Moira contributors"
 
 
