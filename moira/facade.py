790db0db7a2352d95f0fb9996eb8a1ef6f586444
TheDaniel166 <burkettdaniel927@gmail.com>
2026-04-01 16:19:19 -0400

    Release 1.0.0

--- moira/facade.py
+++ moira/facade.py
@@ -1070,7 +1070,7 @@ __all__ = [
 try:
     __version__ = package_version("moira-astro")
 except PackageNotFoundError:
-    __version__ = "0.1.7"
+    __version__ = "1.0.0"
 __author__  = "Moira contributors"
 
 
